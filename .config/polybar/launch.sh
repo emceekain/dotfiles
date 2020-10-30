3217949dec1d8c28e7de00133bf92550a564c220
Brent McKain <emceekain@gmail.com>
2020-10-30 10:27:18 -0500

    Updating polybar to load dynamically (independent of display name(s) on a given system).

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,11 +4,18 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+  done
+else
+  polybar --reload example &
+fi
 # Launch bar1 and bar2
 #polybar bar1 &
 #polybar bar2 &
-polybar top &
+#polybar top &
 #polybar bottom &
-polybar top2 &
+#polybar top2 &
 #polybar bottom2 &
 echo "Bars launched..."
