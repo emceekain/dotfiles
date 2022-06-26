9a4b9bd2fcc4f455e5dec3879761ec29361eda57
Brent McKain <emceekain@gmail.com>
2022-06-25 22:20:22 -0500

    Small bug with stow install overwriting colors.ini.

--- i3/.config/polybar/scripts/colors-dark.sh
+++ i3/.config/polybar/scripts/colors-dark.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Color files
-PFILE="$HOME/.config/polybar/colors.ini"
-RFILE="$HOME/.config/polybar/scripts/rofi/colors.rasi"
+PFILE="$HOME/dotfiles/i3/.config/polybar/colors.ini"
+RFILE="$HOME/dotfiles/i3/.config/polybar/scripts/rofi/colors.rasi"
 
 # Change colors
 change_color() {
